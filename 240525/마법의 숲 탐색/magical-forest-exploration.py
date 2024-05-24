302306fdee7fcd34a57c1605a46ceb5390d8396e
Western (SeokWoo Lee ) <syleelsw@gmail.com>
2024-05-25 01:02:08 +0900

    Title: 마법의 숲 탐색, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240525/마법의 숲 탐색/magical-forest-exploration.py
@@ -0,0 +1,61 @@
+import sys
+input = sys.stdin.readline
+directions = [(-1,0), (0,1), (1,0), (0,-1)]
+R,C,K = map(int,input().split())
+MAP = [[0]*C for _ in range(R+2)]
+def clear():
+    for i in range(R+2):
+        for j in range(C):
+            MAP[i][j] = 0
+inbound = lambda r,c : 0<=r<R+2 and 0<=c<C
+def canGoDown(r,c):
+    if not inbound(r+2,c): return False
+    if not inbound(r+1,c+1) or not inbound(r+1,c-1): return False
+    if (MAP[r+2][c] or MAP[r+1][c+1] or MAP[r+1][c-1]): return False
+    return True
+
+def canGoDownRotateW(r,c):
+    if not inbound(r+2,c-1): return False
+    if not inbound(r+1,c-1) or not inbound(r+1,c-2): return False
+    if not inbound(r,c-2) or not inbound(r-1,c-1): return False
+    if (MAP[r+2][c-1] or MAP[r+1][c-1] or MAP[r+1][c-2]or MAP[r][c-2]or MAP[r-1][c-1]): return False
+    return True
+
+def canGoDownRotateE(r,c):
+    if not inbound(r+2,c+1): return False
+    if not inbound(r+1,c+1) or not inbound(r+1,c+2): return False
+    if not inbound(r,c+2) or not inbound(r-1,c+1): return False
+    if (MAP[r+2][c+1] or MAP[r+1][c+1] or MAP[r+1][c+2]or MAP[r][c+2]or MAP[r-1][c+1]): return False
+    return True
+answer = 0
+for _ in range(K):
+    c,d = map(int,input().split())
+    c -= 1
+    r = 0
+    while 1:
+        if(canGoDown(r,c)):
+            r+=1
+        elif(canGoDownRotateW(r,c)):
+            r += 1
+            c -= 1
+            d = (d-1)%4
+        elif(canGoDownRotateE(r,c)):
+            r += 1
+            c += 1
+            d = (d+1)%4
+        elif r <= 1:
+            clear()
+            break
+        else:
+            dr, dc = directions[d]
+            M = r
+            for ddr, ddc in ((1,0),(-1,0), (0,1), (0,-1)):
+                nr = r+dr+ddr
+                nc = c +dc +ddc
+                if not inbound(nr, nc): continue
+                M = max(MAP[nr][nc],M)
+            for dr, dc in ((1,0),(-1,0), (0,1), (0,-1),(0,0)):
+                MAP[r+dr][c+dc] = M
+            answer += M
+            break
+print(answer)
